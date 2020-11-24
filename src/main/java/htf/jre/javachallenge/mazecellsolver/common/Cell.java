f721cb0bbd5a48995541e47ab580bb60ffc124eb
Shawn <gyina.shawn@gmail.com>
2020-11-24 11:39:38 +0100

    Maze stuff

--- src/main/java/htf/jre/javachallenge/mazecellsolver/common/Cell.java
+++ src/main/java/htf/jre/javachallenge/mazecellsolver/common/Cell.java
@@ -1,10 +1,13 @@
 package htf.jre.javachallenge.mazecellsolver.common;
 
+import htf.jre.javachallenge.mazecellsolver.spatial.Vector;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +23,13 @@ public class Cell {
     private String challengeParameters;
 
     private int prize;
+
+    public Set<Direction> getNonBlockedDirections() {
+        final List<Direction> directionList = sides.stream().map(Direction::valueOf).collect(Collectors.toList());
+        return Direction.list.stream().filter(list -> !directionList.contains(list)).collect(Collectors.toSet());
+    }
+
+    public Vector getVector() {
+        return new Vector(x, y);
+    }
 }
--- src/main/java/htf/jre/javachallenge/mazecellsolver/common/Direction.java
+++ src/main/java/htf/jre/javachallenge/mazecellsolver/common/Direction.java
@@ -2,6 +2,10 @@ package htf.jre.javachallenge.mazecellsolver.common;
 
 
 import htf.jre.javachallenge.mazecellsolver.spatial.Vector;
+import org.springframework.util.StreamUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 public enum Direction {
     NORTH(new Vector(0, -1)), EAST(new Vector(1, 0)), SOUTH(new Vector(0, 1)), WEST(new Vector(-1, 0));
@@ -12,4 +16,10 @@ public enum Direction {
     Direction(Vector vector) {
         this.vector = vector;
     }
+
+    public Vector getVector() {
+        return vector;
+    }
+
+    public static List<Direction> list = Arrays.asList(values());
 }
